7da5bbce9731fb754cc1bbf89d17f08fe4e26fc6
Benjamin W. Portner <benjaminwportner@gmail.com>
2025-05-03 16:03:11 +0200

    add tests

---
+++ test/dag.test.ts
@@ -0,0 +1,48 @@
+import { D3DAGAdapter } from "../src/dag";
+import { SimpleFamilyTree } from "./fixtures";
+import { expect } from "chai";
+
+let dag: D3DAGAdapter;
+const numberPersons = Object.keys(SimpleFamilyTree.persons).length;
+const numberUnions = Object.keys(SimpleFamilyTree.unions).length;
+const numberLinks = SimpleFamilyTree.links.length;
+
+describe("D3DAGAdapter", () => {
+  it("should create a D3DAGAdapter instance", () => {
+    dag = new D3DAGAdapter(SimpleFamilyTree);
+    expect(dag).to.be.instanceOf(D3DAGAdapter);
+  });
+  it("should have a graph property", () => {
+    expect(dag.graph).not.undefined;
+  });
+  it("should contain all nodes and links", () => {
+    expect(dag.graph.nlinks()).to.equal(numberLinks);
+    expect(dag.graph.nnodes()).to.equal(numberPersons + numberUnions);
+  });
+  it("should contain coordinates for all nodes", () => {
+    const nodes = [...dag.graph.nodes()];
+    nodes.forEach((node) => {
+      expect(node).to.have.property("x");
+      expect(node).to.have.property("y");
+      expect(node.x).to.be.a("number");
+      expect(node.y).to.be.a("number");
+    });
+  });
+  it("should contain node metadata", () => {
+    const nodes = [...dag.graph.nodes()];
+    nodes.forEach((node) => {
+      expect(node).to.have.property("data");
+      const data = node.data;
+      expect(data).to.have.property("id");
+      expect(data).to.have.property("type");
+      if (data.type === "person") {
+        expect(data).to.have.property("name");
+        expect(data).to.have.property("birthyear");
+      }
+      if (data.type === "union") {
+        expect(data).to.have.property("partner");
+        expect(data).to.have.property("children");
+      }
+    });
+  });
+});
---
+++ test/fixtures.ts
@@ -0,0 +1,23 @@
+import { readFileSync } from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+import { FamilyTreeData } from "../src/types/types";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Function to remove comments from JSON content
+function removeComments(content: string): string {
+  return content
+    .replace(/\/\/.*$/gm, "") // Remove single-line comments
+    .replace(/\/\*[\s\S]*?\*\//g, ""); // Remove multi-line comments
+}
+
+let fixtureFile = join(__dirname, "../data/data_simple.js");
+let fileContent = readFileSync(fixtureFile, "utf-8").replace("data = ", "");
+export const SimpleFamilyTree: FamilyTreeData = JSON.parse(fileContent);
+
+fixtureFile = join(__dirname, "../data/data.js");
+fileContent = readFileSync(fixtureFile, "utf-8").replace("data = ", "");
+fileContent = removeComments(fileContent);
+export const GoTFamilyTree: FamilyTreeData = JSON.parse(fileContent);
