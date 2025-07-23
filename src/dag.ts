4eb5344d9cfd9ba7e6c6215dc963619dbdd14dc9
Benjamin W. Portner <benjaminwportner@gmail.com>
2025-07-23 15:24:42 +0200

    add D3DAgAdapter options to constructor

--- src/dag.ts
+++ src/dag.ts
@@ -11,16 +11,56 @@ export interface UnionData extends Union {
   parentIds: string[];
 }
 export interface UnionNode extends d3dag.MutGraphNode<UnionData, undefined> {}
-export interface Node extends d3dag.MutGraphNode<PersonData | UnionData, undefined> {}
+export type NodeDatum = PersonData | UnionData;
+export type LinkDatum = undefined;
+export interface Node extends d3dag.MutGraphNode<NodeDatum, LinkDatum> {}
+export interface Link extends d3dag.MutGraphLink<NodeDatum, LinkDatum> {}
 
-export interface Link extends d3dag.MutGraphLink<PersonData | UnionData, undefined> {}
+export interface D3DAGAdapterOptions {
+  nodeSize: [number, number];
+  layering: d3dag.Layering<NodeDatum, LinkDatum>;
+  decross: d3dag.Decross<NodeDatum, LinkDatum>;
+  coord: d3dag.Coord<NodeDatum, LinkDatum>;
+}
+
+function customSugiyamaDecross(
+  layers: d3dag.SugiNode<NodeDatum>[][]
+): void {
+  // apply optimal decrossing algorithm
+  d3dag.decrossOpt()(layers);
+  // then re-arrange to make sure that union partners are next to each other
+  for (let layer of layers) {
+    layer.sort((a, b) => {
+      if (a.data.role === 'link') return 0;
+      if (b.data.role === 'link') return 0;
+      if (a.data.node.data.type === 'union') return 0;
+      if (b.data.node.data.type === 'union') return 0;
+      const aPerson = a.data.node.data as PersonData;
+      const bPerson = b.data.node.data as PersonData;
+      if (aPerson.own_unions?.length === 0) return 1;
+      if (bPerson.own_unions?.length === 0) return -1;
+      return 0;
+    });
+  }
+}
+
+const D3DAGAdapterDefaultOptions: D3DAGAdapterOptions = {
+  nodeSize: [120, 120],
+  layering: d3dag.layeringSimplex(),
+  decross: customSugiyamaDecross,
+  coord: d3dag.coordQuad(),
+};
 
 export class D3DAGAdapter {
   public graph;
   private _width: number = 0;
   private _height: number = 0;
 
-  constructor(data: FamilyTreeData) {
+  constructor(data: FamilyTreeData, userOpts?: D3DAGAdapterOptions) {
+    const opts = {
+      ...D3DAGAdapterDefaultOptions,
+      ...userOpts,
+    };
     const persons = [...Object.values(data.persons)];
     const mappedPersons = persons.map((person: Person) => {
       return {
@@ -47,10 +87,10 @@ export class D3DAGAdapter {
     // calculate the layout
     const layout = d3dag
       .sugiyama()
-      .nodeSize([120, 120])
-      .layering(d3dag.layeringSimplex())
-      .decross(this.customSugiyamaDecross)
-      .coord(d3dag.coordQuad());
+      .nodeSize(opts.nodeSize)
+      .layering(opts.layering)
+      .decross(opts.decross)
+      .coord(opts.coord)
     const layoutResult = layout(this.graph);
     this._width = layoutResult.width;
     this._height = layoutResult.height;
@@ -61,24 +101,5 @@ export class D3DAGAdapter {
   }
   get height() {
     return this._height;
-  }
-
-  private customSugiyamaDecross(layers: d3dag.SugiNode<PersonData | UnionData>[][]): void {
-    // apply optimal decrossing algorithm
-    d3dag.decrossOpt()(layers);
-    // then re-arrange to make sure that union partners are next to each other
-    for (let layer of layers) {
-      layer.sort((a, b) => {
-        if (a.data.role === 'link') return 0;
-        if (b.data.role === 'link') return 0;
-        if (a.data.node.data.type === 'union') return 0;
-        if (b.data.node.data.type === 'union') return 0;
-        const aPerson = a.data.node.data as PersonData;
-        const bPerson = b.data.node.data as PersonData;
-        if (aPerson.own_unions?.length === 0) return 1;
-        if (bPerson.own_unions?.length === 0) return -1;
-        return 0;
-      });
-    }
   }
 }
