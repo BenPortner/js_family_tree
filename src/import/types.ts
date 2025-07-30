d33f8fd627bfc1a7d690b4d3662c3ed1d4ae21a4
Benjamin W. Portner <benjaminwportner@gmail.com>
2025-07-30 10:44:41 +0200

    remove interfaces dependency on d3

--- src/import/types.ts
+++ src/import/types.ts
@@ -1,9 +1,4 @@
-import type {
-  GraphNode as D3GraphNode,
-  GraphLink as D3GraphLink,
-  Graph as D3Graph,
-} from 'd3-dag';
-import type { NodeID, Person, Union } from '../familyTreeData';
+import type { Person, Union } from '../familyTreeData';
 import type { ClickableNode } from '../clickableNode';
 
 export const CPerson = 'person' as const;
@@ -20,10 +15,20 @@ export interface UnionData extends Union {
   insertedNodes: ClickableNode[];
 }
 export type NodeData = PersonData | UnionData;
-export type LinkData = [NodeID, NodeID];
-export type GraphNode = D3GraphNode<NodeData, LinkData>;
-export type GraphLink = D3GraphLink<NodeData, LinkData>;
-export type Graph = D3Graph<NodeData, LinkData>;
+export type LinkData = any;
+
+export interface GraphNode {
+  data: NodeData;
+  children(): IterableIterator<GraphNode>;
+  parents(): IterableIterator<GraphNode>;
+}
+export interface GraphLink {
+  source: GraphNode;
+  target: GraphNode;
+}
+export interface Graph {
+  nodes(): IterableIterator<GraphNode>;
+}
 
 export interface Importer {
   import(data: any): ClickableNode[];
