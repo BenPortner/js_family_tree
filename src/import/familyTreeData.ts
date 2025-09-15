05b3ca8244111acc185ab571116b14ec1da67d8e
Benjamin W. Portner <benjaminwportner@gmail.com>
2025-09-15 16:59:00 +0200

    bug fix: draw root node when there are no links

--- src/import/familyTreeData.ts
+++ src/import/familyTreeData.ts
@@ -1,4 +1,4 @@
-import { graphConnect } from 'd3-dag';
+import { graphConnect, graphStratify } from 'd3-dag';
 import type { FamilyTreeData } from '../familyTreeData';
 import {
   CPerson,
@@ -11,32 +11,68 @@ import {
 import { augmentD3DAGNodeClass, type ClickableNode } from '../clickableNode';
 
 export class FamilyTreeDataV1Importer implements Importer {
-  import(data: FamilyTreeData) {
-    const builder = graphConnect().nodeDatum((id) => {
-      if (id in data.persons) {
-        return {
-          ...data.persons[id],
-          id: id,
-          type: CPerson,
-          visible: id == data.start,
-          insertedBy: null,
-        } as PersonData;
-      } else if (id in data.unions) {
-        return {
-          ...data.unions[id],
-          id: id,
-          type: CUnion,
-          visible: false,
-          insertedBy: null,
-        } as UnionData;
-      } else {
-        throw Error(`ID '${id}' not found in data.persons or data.unions.`);
-      }
-    });
-    const graph = builder(data.links) as Graph;
+  import(data: FamilyTreeData): ClickableNode[] {
+    let graph: Graph;
+    if (data.links && data.links.length > 0) {
+      graph = this.buildGraphFromLinks(data);
+    } else {
+      graph = this.buildGraphFromParentIds(data);
+    }
     const nodes = [...graph.nodes()];
     // add custom methods (augment)
     augmentD3DAGNodeClass(nodes[0]);
     return nodes as ClickableNode[];
+  }
+
+  private buildGraphFromLinks(data: FamilyTreeData) {
+    const builder = graphConnect().nodeDatum((id) => {
+      if (id in data.persons) {
+        const person = data.persons[id];
+        Object.assign(person, {
+          id: id,
+          type: CPerson,
+          visible: (person as PersonData).visible ?? id == data.start,
+          insertedBy: (person as PersonData).insertedBy ?? null,
+        });
+        return person as PersonData;
+      } else if (id in data.unions) {
+        const union = data.unions[id];
+        Object.assign(union, {
+          ...data.unions[id],
+          id: id,
+          type: CUnion,
+          visible: (union as UnionData).visible ?? false,
+          insertedBy: (union as UnionData).insertedBy ?? null,
+        });
+        return union as UnionData;
+      } else {
+        throw Error(`ID '${id}' not found in data.persons or data.unions.`);
+      }
+    });
+    return builder(data.links) as Graph;
+  }
+
+  private buildGraphFromParentIds(data: FamilyTreeData) {
+    const builder = graphStratify();
+    const personArr = Object.entries(data.persons).map(([id, person]) => {
+      Object.assign(person, {
+        id: id,
+        type: CPerson,
+        visible: (person as PersonData).visible ?? id == data.start,
+        insertedBy: (person as PersonData).insertedBy ?? null,
+      });
+      return person as PersonData;
+    });
+    const unionArr = Object.entries(data.unions).map(([id, union]) => {
+      Object.assign(union, {
+        id: id,
+        type: CUnion,
+        visible: (union as UnionData).visible ?? false,
+        insertedBy: (union as UnionData).insertedBy ?? null,
+      });
+      return union as UnionData;
+    });
+    const allNodes = [...personArr, ...unionArr];
+    return builder(allNodes) as Graph;
   }
 }
